0898666df7f921a5d9bca64a051d6cae2862b563
OriginalAnswer <akiseonair@gmail.com>
2022-01-03 14:12:19 +0900

    Create

---
+++ flipbook.js
@@ -0,0 +1,32 @@
+    var numberOfPages = 1000;
+function addPage(page, book) {
+    if (!book.turn('hasPage', page)) {
+        var element = $('<div />', { 'class': 'page ' + ((page % 2 == 0) ? 'odd' : 'even'), 'id': 'page-' + page }).html('<i class="loader"></i>');
+        book.turn('addPage', element, page);
+        setTimeout(function () { element.html('<div class="data">Data for page ' + page + '</div>'); }, 1000);
+    }
+} $(window).ready(function () {
+    $('#book').turn({
+        acceleration: true,
+        pages: numberOfPages,
+        elevation: 50,
+        gradients: !$.isTouch,
+        when: {
+            turning: function (e, page, view) {
+                var range = $(this).turn('range', page);
+                for (page = range[0]; page <= range[1]; page++) addPage(page, $(this));
+            }, turned: function (e, page) { $('#page-number').val(page); }
+        }
+    }); $('#number-pages').html(numberOfPages);
+    $('#page-number').keydown(function (e) {
+        if (e.keyCode == 13)
+            $('#book').turn('page', $('#page-number').val());
+    });
+});
+$(window).bind('keydown', function (e) {
+    if (e.target && e.target.tagName.toLowerCase() != 'input')
+        if (e.keyCode == 37)
+            $('#book').turn('previous');
+        else if (e.keyCode == 39)
+            $('#book').turn('next');
+});
